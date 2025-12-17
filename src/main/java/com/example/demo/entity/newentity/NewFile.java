f11cd246bbe7bdad0e18553e70a538b00c7c3a72
bala-amypo <bala@amypo.in>
2025-12-17 08:21:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/newentity/NewFile.java
+++ src/main/java/com/example/demo/entity/newentity/NewFile.java
@@ -2,7 +2,8 @@ package com.example.project.newentity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constrains.*;
-
+@Entity
+@Table(name="Student")
 public class NewFile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,5 +13,30 @@ public class NewFile {
     @NotBlank(message="no blank allowed")
     @Email(message="invalid format")
     private String email;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Newentity(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public Newentity() {
+    }
     
 }
